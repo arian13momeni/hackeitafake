3aa4e52214cd18cd53c2931674d424125776b8e9
ariAN <114738229+arian13momeni@users.noreply.github.com>
2022-12-14 13:11:45 +0330

    Create home.py

---
+++ home.py
@@ -0,0 +1,13 @@
+
+print("macke whit love and Coffee ")
+print("Welcome to the eita hack")
+print("------------------------------------------------------------------------------------");
+print("1.chanell hack")
+print("2. member hack")
+print("\n")
+choose = input("Choose an option:")
+
+if (str(choose) == "1"):
+   print(figlet_format("hack chanell"))
+elif (str(choose) == "2"):
+    print(figlet_format("hackmember"))
